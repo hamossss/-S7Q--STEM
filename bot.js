1546b09e436cfe88f5aa55e4b73e858772b92f6d
hamossss <40631448+hamossss@users.noreply.github.com>
2018-07-08 03:39:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -293,42 +293,6 @@ client.on('message', message => {
 });
 
 
-const Eris = require("eris");
-var bot = new Eris("NDY0MjAwNTk5NTcyMDU0MDE5.Dh7giw.0nnQGjLpPpJgqyY5eipkYojgXXM");
-var id = "459926458626736130";
-//** لاتعدل على شي تحت ذا **//
-bot.on("ready", () => {
-onstart();
-});
 
-function onstart(){
-
-setInterval(function(){
-
-var currentTime = new Date(),
-hours = currentTime.getHours() + 0 ,
-minutes = currentTime.getMinutes(),
-seconds = currentTime.getSeconds();
-
-if (minutes < 10) {
-minutes = "0" + minutes;
-}
-var suffix = "صباحاَ";
-if (hours >= 12) {
-suffix = "مساء";
-hours = hours - 12;
-}
-if (hours == 0) {
-hours = 12;
-}
-
-bot.editChannel(id, { name : "●⌠ " + "الوقت : " + hours + ":" + minutes + " " + suffix + " ⌡●"});
-}, 60000);
-
-}
-
-bot.connect();
-
-bot.connect();
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
